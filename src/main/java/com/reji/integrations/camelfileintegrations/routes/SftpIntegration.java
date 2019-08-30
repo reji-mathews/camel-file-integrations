eae436936918a79ebac6f4735454083695f949d9
Reji <contactreji@gmail.com>
2019-08-30 19:08:35 -0400

    baseline

--- src/main/java/com/reji/integrations/camelfileintegrations/routes/SftpIntegration.java
+++ src/main/java/com/reji/integrations/camelfileintegrations/routes/SftpIntegration.java
@@ -1,7 +1,9 @@
 package com.reji.integrations.camelfileintegrations.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SftpIntegration extends RouteBuilder {
     @Override
     public void configure() throws Exception {
